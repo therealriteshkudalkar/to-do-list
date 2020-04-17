facc1010071064b2205df06d82b6e0c802acda2d
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2020-04-17 19:26:07 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  
+});
+
+app.listen(3000, function() {
+  console.log("The server is now listening on port 3000.");
+});
