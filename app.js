a0fa5e5d6084d3777c4b44031e542cf4bb8ae2a4
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2020-04-18 09:18:36 +0530

    Added basic functionality of a to-do-list.

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+var items = [];
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+  const date = new Date().toLocaleString("en-us", options);
+  res.render("index", {date: date, items: items});
 });
 
 app.post("/", function(req, res) {
-  
+  items.push(req.body.nextItem);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
